a2bcd4d3d2337b263d191f7635bb31a5ef34e094
Marcin Kuthan <marcin.kuthan@gmail.com>
2020-04-19 19:29:42 +0200

    First minimal working version.

--- README.md
+++ README.md
@@ -1,2 +1,164 @@
-# gcp-minecraft
+# minecraft-gcp
+
 Minecraft Server automation on GCP
+
+Features:
+* Server status/start/stop pages (by [Cloud Functions](https://cloud.google.com/functions/docs))
+* Automated server shutdown if there is no active players (scheduled by [Cloud Scheduler](https://cloud.google.com/scheduler/docs))
+* Minecraft [RCON](https://wiki.vg/RCON) password secured in [Google Secret Manager](https://cloud.google.com/secret-manager/docs)
+* Minecrart world synchronized to/from [Google Storage](https://cloud.google.com/storage/docs)
+
+TODO list:
+* Docker image instead of startup-script / shutdown-script
+* DNS
+* Compute instance cost reduction
+
+## Local environment
+
+Use the `venv` command to create a virtual copy of the entire Python installation. 
+
+```shell script
+python -m venv venv
+```
+
+Set your shell to use the `venv` paths for Python by activating the virtual environment.
+
+```shell script
+source venv/bin/activate
+```
+
+Install packages (see [pre-installed packages](https://cloud.google.com/functions/docs/writing/specifying-dependencies-python) for recommended versions).
+
+```shell script
+pip install -r cloud-function/requirements.txt
+```
+
+## Secret manager
+
+TODO
+
+## Cloud scheduler
+
+Create an App Engine app within the current Google Cloud Project.
+
+```shell script
+gcloud app create --region=europe-west
+```
+
+Create PubSub topic for scheduled events.
+
+```shell script
+gcloud pubsub topics create ten-minutes-jobs
+```
+
+Schedule events published to PubSub topic every ten minutes.
+
+```shell script
+gcloud scheduler jobs create pubsub ten-minutes-jobs \
+  --schedule "*/10 * * * *" --topic ten-minutes-jobs --message-body " "
+```
+
+Cron job could be paused and resumed.
+
+```shell script
+gcloud scheduler jobs pause ten-minutes-jobs
+```
+
+```shell script
+gcloud scheduler jobs resume ten-minutes-jobs
+```
+
+## Google storage
+
+TODO: setup bucket and configure minecraft distro
+
+## Compute instance
+
+TODO:
+https://cloud.google.com/solutions/gaming/minecraft-server
+
+Startup script.
+
+```shell script
+#! /bin/bash
+
+# install java & screen
+apt-get update && apt-get install -y default-jre-headless screen
+
+# install minecraft from GS
+mkdir -p /minecraft
+
+gsutil -m rsync -x ".*\.log\.gz$" -r gs://minecraft-272917 /minecraft
+
+# start minecraft
+cd /minecraft
+screen -d -m -S mcs java -Xms1G -Xmx3G -d64 -jar server.jar nogui
+```
+
+Shutdown script.
+
+```shell script
+#! /bin/bash
+
+# stop minecraft 
+screen -r mcs -X stuff '/stop\n'
+
+# save minecraft installation to GS
+gsutil -m rsync -x ".*\.log\.gz$" -r /minecraft gs://minecraft-272917
+```
+
+## Cloud functions
+
+Create stage bucket for cloud functions.
+
+```shell script
+gsutil mb -c regional -l europe-west1 gs://minecraft-272917-cloud-functions
+```
+
+Add `secretmanager.secretAccessor` role to service account.
+
+```shell script
+TODO
+```
+
+Deploy cloud functions (see [deploy manual](https://cloud.google.com/sdk/gcloud/reference/functions/deploy)).
+
+Server status page.
+
+```shell script
+gcloud functions deploy server_status \
+  --timeout=180s --max-instances=1 \
+  --region=europe-west1 \
+  --stage-bucket=minecraft-272917-cloud-functions \
+  --source=./cloud-function --runtime=python37 --trigger-http 
+```
+
+Start server page.
+
+```shell script
+gcloud functions deploy start_server \
+  --timeout=300s --max-instances=1 \
+  --region=europe-west1 \
+  --stage-bucket=minecraft-272917-cloud-functions \
+  --source=./cloud-function --runtime=python37 --trigger-http
+```
+
+Stop server page.
+
+```shell script
+gcloud functions deploy stop_server \
+  --timeout=300s --max-instances=1 \
+  --region=europe-west1 \
+  --stage-bucket=minecraft-272917-cloud-functions \
+  --source=./cloud-function --runtime=python37 --trigger-http
+```
+
+Automated server stopping handler.
+
+```shell script
+gcloud functions deploy stop_server_handler \
+  --timeout=300s --max-instances=1 \
+  --region=europe-west1 \
+  --stage-bucket=minecraft-272917-cloud-functions \
+  --source=./cloud-function --runtime=python37 --trigger-topic ten-minutes-jobs
+```
---
+++ cloud-function/main.py
@@ -0,0 +1,183 @@
+import re
+import time
+
+import googleapiclient.discovery
+import mcrcon
+from flask import render_template
+from google.cloud import secretmanager
+
+PROJECT = "minecraft-272917"
+ZONE = "europe-west1-b"
+SERVER_NAME = "minecraft-server-2"
+RCON_SECRET_KEY = "rcon"
+
+
+class SecretClient(object):
+    def __init__(self, project):
+        self.project = project
+        self.client = secretmanager.SecretManagerServiceClient()
+
+    def get_secret(self, key):
+        secret_version = "projects/{0}/secrets/{1}/versions/latest".format(self.project, key)
+        secret = self.client.access_secret_version(secret_version)
+        return secret.payload.data.decode('UTF-8')
+
+
+class ComputeClient(object):
+    def __init__(self, project, zone):
+        self.project = project
+        self.zone = zone
+        self.client = googleapiclient.discovery.build("compute", "v1")
+
+    def find_instance(self, name):
+        result = self.client.instances().list(project=self.project, zone=self.zone, filter="name = " + name).execute()
+        return ComputeInstance(result["items"][0])
+
+    def get_instance(self, id):
+        result = self.client.instances().get(project=self.project, zone=self.zone, instance=id).execute()
+        return ComputeInstance(result)
+
+    def stop_instance(self, instance):
+        operation = self.client.instances().stop(project=self.project, zone=self.zone, instance=instance.get_id()).execute()
+        self._wait_for_operation(operation)
+        return self.get_instance(instance.get_id())
+
+    def start_instance(self, instance):
+        operation = self.client.instances().start(project=self.project, zone=self.zone, instance=instance.get_id()).execute()
+        self._wait_for_operation(operation)
+        return self.get_instance(instance.get_id())
+
+    def _wait_for_operation(self, operation):
+        while True:
+            result = self.client.zoneOperations().get(project=self.project, zone=self.zone, operation=operation["name"]).execute()
+
+            if result['status'] == 'DONE':
+                if 'error' in result:
+                    raise Exception(result['error'])
+                return result
+
+            time.sleep(1)
+
+
+class ComputeInstance(object):
+    def __init__(self, item, players=None):
+        self.item = item
+        self.players = players
+
+    def __str__(self):
+        return str(self.item)
+
+    def get_id(self):
+        return self.item["id"]
+
+    def get_name(self):
+        return self.item["name"]
+
+    def get_ip(self):
+        try:
+            return self.item["networkInterfaces"][0]["accessConfigs"][0]["natIP"]
+        except KeyError:
+            return None
+
+    def get_status(self):
+        return self.item["status"]
+
+    def is_running(self):
+        return self.get_status() == "RUNNING"
+
+    def is_terminated(self):
+        return self.get_status() == "TERMINATED"
+
+    def is_done(self):
+        return (self.is_running() and self.players) or self.is_terminated()
+
+
+def server_status(request):
+    compute_client = ComputeClient(PROJECT, ZONE)
+    instance = compute_client.find_instance(SERVER_NAME)
+    if instance.is_running():
+        message = "started"
+    elif instance.is_terminated():
+        message = "stopped"
+    else:
+        message = instance.get_status().lower()
+    return _render_server_status(instance, message)
+
+
+def start_server(request):
+    compute_client = ComputeClient(PROJECT, ZONE)
+    instance = compute_client.find_instance(SERVER_NAME)
+    if instance.is_terminated():
+        instance = compute_client.start_instance(instance)
+
+    if instance.is_running():
+        message = "started"
+    else:
+        message = "is starting ..."
+    return _render_server_status(instance, message)
+
+
+def stop_server(request):
+    compute_client = ComputeClient(PROJECT, ZONE)
+    instance = compute_client.find_instance(SERVER_NAME)
+    if instance.is_running():
+        instance = compute_client.stop_instance(instance)
+
+    if instance.is_terminated():
+        message = "stopped"
+    else:
+        message = "is stopping ..."
+
+    return _render_server_status(instance, message)
+
+
+def stop_server_handler(event, context):
+    compute_client = ComputeClient(PROJECT, ZONE)
+    instance = compute_client.find_instance(SERVER_NAME)
+    if instance.is_running():
+        number_of_players = _minecraft_number_of_players(instance)
+        if number_of_players == 0:
+            print("Stopping instance {} without active players".format(instance.get_name()))
+            compute_client.stop_instance(instance)
+        else:
+            print("Instance {} is used by {} player(s), skip stopping".format(instance.get_name(), number_of_players))
+
+
+def _render_server_status(instance, message):
+    if instance.is_running():
+        players = _minecraft_list_players(instance)
+        if players is None:
+            message = "is starting ..."
+        else:
+            instance.players = players
+
+    return render_template('status.html', message=message, instance=instance)
+
+
+def _minecraft_list_players(instance):
+    return _minecraft_command(instance, "/list")
+
+
+def _minecraft_number_of_players(instance):
+    list = _minecraft_command(instance, "/list")
+    if list is None:
+        return 0
+    else:
+        number_of_players = re.findall("There are (\d+) of a max \d+ players online", list)[0]
+        return int(number_of_players)
+
+
+def _minecraft_command(instance, command):
+    try:
+        secret_client = SecretClient(PROJECT)
+        with mcrcon.MCRcon(instance.get_ip(), secret_client.get_secret(RCON_SECRET_KEY)) as client:
+            return client.command(command)
+    except ConnectionRefusedError:
+        return None
+
+
+if __name__ == "__main__":
+    # compute_client = ComputeClient(PROJECT, ZONE)
+    # compute_instance = compute_client.find_instance(SERVER_NAME)
+    # print(compute_instance.get_status())
+    pass
